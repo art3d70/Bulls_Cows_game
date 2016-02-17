448102e7714062bf522147faf8a30a39b3d1734c
Arturo Benitez <art3d70@gmail.com>
2016-02-17 16:10:17 -0600

    Clase 14

--- Game_001/Bulls_Cows_art/main.cpp
+++ Game_001/Bulls_Cows_art/main.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main() {
-
+	constexpr int WORD_LENGTH = 9;
 	cout << "Bienvenido al Juego" << endl;
+	cout << "Adivina la palabra de " << WORD_LENGTH << " letras" << endl;
 	return 0;
+
 }
