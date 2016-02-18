b86bd8ce2982637a7f95a1c09e2e62df2e350ba6
Arturo Benitez <art3d70@gmail.com>
2016-02-18 00:29:40 -0600

    Clase 20

--- Game_001/Bulls_Cows_art/main.cpp
+++ Game_001/Bulls_Cows_art/main.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+void PrintIntro();
+void PlayGame();
+string GetGuess();
+
+// the entry point of  the app
 int main() {
+
+	PrintIntro();
+	PlayGame();
+
+	return 0;
+}
+
+void PrintIntro() {
 
 	constexpr int WORD_LENGTH = 9;
 	cout << "Bienvenido al Juego" << endl;
 	cout << "Adivina la palabra de " << WORD_LENGTH << " letras" << endl;
-	cout << "Tu palabra: " ;
-	
+	return;
+}
+
+void PlayGame() {
+
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		string Guess = GetGuess();
+		cout << "Tu palabra fue: " << Guess << endl;
+		cout << endl;
+	}
+
+}
+
+string GetGuess() {
+
+	cout << "Tu palabra: ";
 	string Guess = "";
 	getline(cin, Guess);
-
-	cout << "Tu palabra fue: " << Guess << endl;
-
-	return 0;
-
+	return Guess;
 }
