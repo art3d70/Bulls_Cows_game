195e63c3f73d5622f6277c7637da4e0b52e2f3de
Arturo Benitez <art3d70@gmail.com>
2016-02-18 00:01:26 -0600

    Clase 15

--- Game_001/Bulls_Cows_art/main.cpp
+++ Game_001/Bulls_Cows_art/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main() {
+
 	constexpr int WORD_LENGTH = 9;
 	cout << "Bienvenido al Juego" << endl;
 	cout << "Adivina la palabra de " << WORD_LENGTH << " letras" << endl;
+	cout << "Tu palabra: " ;
+	
+	string Guess = "";
+	getline(cin, Guess);
+
+	cout << "Tu palabra fue: " << Guess << endl;
+
 	return 0;
 
 }
